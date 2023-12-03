24682534c519c518ba17ebabb82ecd2f6b35cdef
takatoshi.tenko <chanten811@gmail.com>
2023-12-03 19:36:17 +0900

    add: 261B問題回答

--- 261.js
+++ 261.js
@@ -1,3 +1,4 @@
+// A問題
 function Main(input) {
   const arg = input.trim().split('\n')
   // const answer = 0
@@ -10,6 +11,78 @@ function Main(input) {
   for (let i = 0; i < redPoint; i++) {
     redLength += firstRed
   }
+  return redLength
+}
+// console.log(Main('0 3 1 5'))
+
+
+// B問題
+// マス分のテーブルをループ処理する
+// 例：
+// 1,2 がWの時、2,1 はL になる
+// 1,2 がLの時、2,1 はW になる
+// 1,2 Dが時、2,1 はDになる
+// 1,1 の時-になる
+// 上記の条件が破綻したらincorrect, 破綻しなかったらcorrectを出力
+
+function checkTournamentResult(input) {
+  const table = input.trim().split('\n')
+  const onlyTable = table.slice(1)
+  const result = onlyTable.map((item) => {
+    return item.split('')
+  })
+
+  for (let i = 0; i < table[0]; i++) {
+    for (let j = 0; j < result[i].length; j++) {
+      if (result[i] === result[j]) {
+        if (result[i][j] !== '-') return console.log('incorrect')
+      }
+      if (result[i][j] === 'W') {
+        if (result[j][i] !== 'L') return console.log('incorrect')
+      }
+      if (result[i][j] === 'L') {
+        if (result[j][i] !== 'W') return console.log('incorrect')
+      }
+      if (result[i][j] === 'D') {
+        if (result[j][i] !== 'D') return console.log('incorrect')
+      }
+    }
+  }
+  return console.log('correct')
 }
 
-console.log(Main('0 3 1 5'))
+function checkTournamentResult(input) {
+  const lines = input.trim().split('\n');
+  const N = parseInt(lines[0]);
+  const results = lines.slice(1).map(item => item.split(''));
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (i !== j) {
+        const outcome = results[i][j];
+
+        if (
+          (outcome === 'W' && results[j][i] !== 'L') ||
+          (outcome === 'L' && results[j][i] !== 'W') ||
+          (outcome === 'D' && results[j][i] !== 'D')
+        ) {
+          return console.log('incorrect');
+        }
+      } else if (results[i][j] !== '-') {
+        return console.log('incorrect');
+      }
+    }
+  }
+
+  return console.log('correct');
+}
+
+// Example usage
+const input = '4\n-WLD\nL-WD\nWL-D\nDL-W';
+checkTournamentResult(input);
+
+console.log(checkTournamentResult(`
+2
+-D
+D-
+`))
